5acda74b78e2ac3768130fa37d6210c13bde6b3e
mario Lafuente <45186223+mlafuentecr@users.noreply.github.com>
2019-04-20 15:14:36 -0600

    parcel ver 1

---
+++ src/index.js
@@ -0,0 +1,6 @@
+
+// import './assets/css/layout/bootstrap.min.css';
+// import './assets/css/layout/style.css';
+// import './assets/css/helpers/animate.min.css';
+import './assets/css/main.scss'; 
+
